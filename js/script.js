3c43d805becf280bbfc67bb7355da641597cfe68
ributwiboworahayu <bowo.william17@gmail.com>
2022-06-20 15:13:07 +0700

    laporan PKL

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let i = 0;
+$('.text-danger').hide();
+$('.detail-modal-button').click(function () {
+    console.log('clicked');
+    $('.card-body').html(load_doc());
+    i++;
+    if (i == 1) {
+        $('.text-danger').show();
+    }
+});
+
+function load_doc() {
+
+    return `
+                    <iframe src="https://docs.google.com/viewerng/viewer?url=https://github.com/ributwiboworahayu/laporan-pkl/blob/main/files/ribut-laporan-PKL.docx?raw%3Dtrue&embedded=true" width="100%" height="600px" frameborder="0"></iframe>
+                    `;
+}
