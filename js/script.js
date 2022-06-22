31f4f50188912db2e9eda0806dc5df27caaecb40
ributwiboworahayu <bowo.william17@gmail.com>
2022-06-22 07:34:05 +0700

    laporan PKL

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 $('.text-danger').hide();
 $('.detail-modal-button').click(function () {
     console.log('clicked');
-    $('.card-body').html(load_doc());
-    $('.text-danger').show();
-    setTimeout(function () {
-        $('.text-danger').hide();
-    }, 2700);
+    $('.detail-modal-button').html(loading());
+    load_data();
+    // location.href = 'https://docs.google.com/viewer?url=https://github.com/ributwiboworahayu/laporan-pkl/blob/main/files/ribut-laporan-PKL.docx?raw=true&embedded=true';
+    // $('.card-body').html(load_doc());
+    // $('.text-danger').show();
+    // setTimeout(function () {
+    //     $('.text-danger').hide();
+    // }, 2700);
 
 });
 
 function load_doc() {
 
     return `
-                    <iframe src="https://docs.google.com/viewerng/viewer?url=https://github.com/ributwiboworahayu/laporan-pkl/blob/main/files/ribut-laporan-PKL.docx?raw%3Dtrue&embedded=true" width="100%" height="600px" frameborder="0"></iframe>
+                    <embed src="https://docs.google.com/viewer?url=https://github.com/ributwiboworahayu/laporan-pkl/blob/main/files/ribut-laporan-PKL.docx?raw=true&embedded=true" width="100%" height="600px" frameborder="0"></embed>
                     `;
 }
+
+function load_data(){
+    location.href = 'https://docs.google.com/viewer?url=https://github.com/ributwiboworahayu/laporan-pkl/blob/main/files/ribut-laporan-PKL.docx?raw=true&embedded=true';
+}
+
+function loading(){
+    return `
+    <div class="loader"></div>
+    `;
+}
+
+
+
+// https://docs.google.com/viewerng/viewer?url=https://github.com/ributwiboworahayu/laporan-pkl/blob/main/files/ribut-laporan-PKL.docx?raw%3Dtrue&embedded=true
